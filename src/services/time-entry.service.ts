62c4a71c3028f37ca0e1aad71fc5b43ebf1f0509
UtsavSa <sapkot_u1@denison.edu>
2025-08-23 00:34:54 -0400

    new fix

--- src/services/time-entry.service.ts
+++ src/services/time-entry.service.ts
@@ -17,9 +17,9 @@ export class TimeEntryService {
 
   //private readonly apiUrl = 'https://localhost:7224/api/timeentry';
   
-  
 
   // prod setting
+
   private readonly api = (environment.apiUrl ?? '').replace(/\/+$/, '');
   private readonly apiUrl =  `${this.api}/api/timeentry`;
   constructor(private http: HttpClient) {}
